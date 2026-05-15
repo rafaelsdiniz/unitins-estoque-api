a36a55b56a6aa5f3ad41b9c10a864dac99a64ce4
rafaelsdiniz <rafaeldiniz@unitins.br>
2026-05-15 11:35:25 -0300

    inicial commit

---
+++ src/main/java/unitins/gestao/estoqueIA/EstoqueIaApplication.java
@@ -0,0 +1,13 @@
+package unitins.gestao.estoqueIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unitins/gestao/estoqueIA/EstoqueIaApplicationTests.java
@@ -0,0 +1,13 @@
+package unitins.gestao.estoqueIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
